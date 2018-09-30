97c9b894fec436d3d09c68e77c610590e4ac74be
Gagandeep Singh <gaganmanku96@gmail.com>
2018-09-30 21:03:58 +0530

    initial commit

---
+++ GUI.py
@@ -0,0 +1,71 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPlainTextEdit, QLabel, QPushButton
+import pickle
+import sys
+from sklearn.feature_extraction.text import CountVectorizer
+import re
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+with open('model.pickle','rb') as f:
+      model = pickle.load(f)
+
+with open('countVector.pickle','rb') as f:
+      cv = pickle.load(f)
+      
+with open('tfidf.pickle','rb') as f:
+      tfidf = pickle.load(f)      
+      
+        
+
+
+class window(QMainWindow):
+      def __init__(self):
+            super().__init__()
+            self.title = "Restaurant Reviews"
+            self.review = 'This is a very good restaurant'
+            self.initialize()
+            self.input()
+            self.show()
+
+      def initialize(self):
+            self.setFixedSize(600,400)
+            self.setWindowTitle = self.title
+            label = QLabel("Write your review",self)
+            label.move(20,10)
+
+            calculateReview = QPushButton("Compute Review",self)
+            calculateReview.move(500,300)
+            calculateReview.clicked.connect(self.process)
+
+            
+      def input(self):
+            text = QPlainTextEdit(self)
+            text.move(20,40)
+            text.resize(400,200)  
+            #self.review = QPlainTextEdit.toPlainText
+             
+
+      def process(self):
+            review = re.sub('[^a-zA-Z]',' ',self.review)
+            lemmatizer = WordNetLemmatizer()
+
+            review = [lemmatizer.lemmatize(word) for word in self.review]
+            review = ''.join(review)
+           
+            li = [review] 
+            print(type(li))
+            
+            
+            #cv= CountVectorizer(max_features=1200)
+            
+                        
+            
+            res = cv.transform(review)
+            #print(res)
+            #res = model.predict(res)
+            #print(res)
+            
+
+
+qt = QApplication(sys.argv)
+w = window()
+sys.exit(qt.exec())            
---
+++ main.py
@@ -0,0 +1,63 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+import re
+df = pd.read_csv('Restaurant_Reviews.tsv',delimiter = '\t')
+
+corpus = []
+
+for i in range(0,1000):
+      review = re.sub('[^a-zA-Z]',' ',df['Review'][i])
+      
+      review = review.lower()
+      review = review.split()  
+      
+    
+      lemmatizer = WordNetLemmatizer()
+  
+       
+      review = [lemmatizer.lemmatize(word) for word in review 
+                if not word in set(stopwords.words('english'))]  
+                  
+      review = ' '.join(review)   
+      corpus.append(review)  
+
+
+vectorizer = CountVectorizer(max_features=1200,)
+X= vectorizer.fit_transform(corpus).toarray()
+y= df.iloc[:,1].values
+
+tfidf = TfidfTransformer()
+X = tfidf.fit_transform(X)
+
+
+from sklearn.cross_validation import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25) 
+
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=510)
+classifier.fit(X_train,y_train)
+pred = classifier.predict(X_test)
+
+
+#from sklearn.metrics import confusion_matrix
+#ac = confusion_matrix(y_test,pred)
+
+
+from sklearn.metrics import accuracy_score
+score = accuracy_score(y_test,pred)
+
+import pickle
+with open('model.pickle','wb') as f:
+      pickle.dump(classifier,f)
+      
+with open('countVector.pickle','wb') as f:
+      pickle.dump(CountVectorizer,f)
+      
+with open('tfidf.pickle','wb') as f:
+      pickle.dump(tfidf,f)
+      
+
